223c50bfe951a046761e26bb8d08504624b8fc7f
DarkzoneDarkzone <flookes2541@gmail.com>
2025-03-27 17:20:33 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VCalendar from 'v-calendar';
 
 Vue.config.productionTip = false
+
+
+// ใช้งาน VCalendar ทั่วทั้งแอป
+Vue.use(VCalendar);
 
 new Vue({
   render: h => h(App),
